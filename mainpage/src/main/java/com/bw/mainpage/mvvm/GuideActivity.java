83a6fd28e8e91169cffc5a25d8b459b5ce069d12
KforikT <1297381637@qq.com>
2021-08-21 08:27:37 +0800

    first

--- mainpage/src/main/java/com/bw/mainpage/mvvm/GuideActivity.java
+++ mainpage/src/main/java/com/bw/mainpage/mvvm/GuideActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.bw.mainpage.R;
+
 public class GuideActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        //孔晨亮
     }
 }
